df0ae09e6b6730d5d59db55d8f3bdb6cdbf62cd1
Alex Chalk <alexchalk17@gmail.com>
2017-08-05 12:06:15 -0400

    Increment a post's likes when like button is clicked

--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -1,7 +1,16 @@
 function posts(state = [], action){
-  console.log('The post will change');
-  console.log(state, action);
-  return state;
+  switch(action.type) {
+    case 'INCREMENT_LIKES' :
+      console.log('Incrementing likes');
+      const i = action.index;
+      return [
+        ...state.slice(0,i), //up to the post we are changing
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1), //after the post we are changing
+      ];
+    default:
+      return state;
+  }
 }
 
 export default posts;
