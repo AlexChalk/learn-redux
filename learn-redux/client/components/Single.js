fa2eb9b9548204a0d4acdd7d25040a689ea1e328
Alex Chalk <alexchalk17@gmail.com>
2017-08-06 10:39:05 -0400

    Build out 'single photo' display
    A single photo is displayed with all likes and comments.

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
+    const { postID } = this.props.params;
+    const i = this.props.posts.findIndex((post) => post.code === postID);
+    const post = this.props.posts[i];
+    const postComments = this.props.comments[postID] || [];
     return (
       <div className="single-photo">
-        I'm the single
+        <Photo i={i} post={post} {...this.props} />
+        <Comments postComments={postComments} />
       </div>
     );
   }
